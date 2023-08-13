e7639f6b6d096f164113330f74ea684117b8af8d
Cranky Bawler <6666lenia6666@mail.ru>
2023-08-13 15:29:22 +0300

    1

--- src/main/java/pro/sky/API/SwaggerUI/Postman/scripts422.sql
+++ src/main/java/pro/sky/API/SwaggerUI/Postman/scripts422.sql
@@ -1,25 +1,29 @@
+create table vehicle (
+                         id serial primary key ,
+                         brand varchar(225),
+                         model varchar(225),
+                         cost money
+                         );
+ALTER TABLE vehicle
+    ADD PRIMARY KEY (id);
+
+
 create table person (
                         id serial primary key ,
                         name varchar(225),
                         age int,
                         vehicle_owner boolean);
+                        vehicle_id int REFERENCES vehicle (id)
+                        );
 
-create table vehicle (
-                         id serial primary key ,
-                         brand varchar(225),
-                         model varchar(225),
-                         cost money);
 
-alter table person add column vehicle_id serial;
-
-alter table vehicle add constraint unique_vehicle unique (model);
 
 
 
 select student.name, student.age, faculty.name
 from student
-         inner join faculty on student.faculty_id = faculty.id;
+         join faculty on student.faculty_id = faculty.id;
 
 select s.id, s.name, avtr.id
 from student as s
-         inner join avatar avtr on s.id = avtr.student_id;
+         join avatar avtr on s.id = avtr.student_id;
